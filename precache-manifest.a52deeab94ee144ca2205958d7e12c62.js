e2e1bb2b58fad93574511a93681b9aa322556c45
imagi2rey <amandeepgujjar0001@gmail.com>
2022-05-02 10:19:20 +0000

    Updates

---
+++ precache-manifest.a52deeab94ee144ca2205958d7e12c62.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "166d9bf26cd60fa878ca8f78466edd18",
+    "url": "/index.html"
+  },
+  {
+    "revision": "7ebe0c96a0c2a923422e",
+    "url": "/static/css/main.ebfde1ba.chunk.css"
+  },
+  {
+    "revision": "d6eea6313479556aafce",
+    "url": "/static/js/2.28efb9d8.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/static/js/2.28efb9d8.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "7ebe0c96a0c2a923422e",
+    "url": "/static/js/main.ed0e7ee2.chunk.js"
+  },
+  {
+    "revision": "fabc6fd72e82fa054f36",
+    "url": "/static/js/runtime-main.c9565b16.js"
+  }
+]);
